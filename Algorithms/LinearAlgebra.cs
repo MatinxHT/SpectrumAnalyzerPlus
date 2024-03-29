f0ed1499fc814cc707b9dc9860a795e220f89fe9
MatinxHT <linduanpeng@live.com>
2024-03-29 23:49:08 +0800

    调整函数分类

---
+++ Algorithms/LinearAlgebra.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectrumAnalyzerPlus.Algorithms
+{
+    public class LinearAlgebra
+    {
+        /// <summary>
+        /// 根据峰数n构建n*n初始矩阵的函数
+        /// </summary>
+        /// <param name="pixeLlist">锋对应的像素序号</param>
+        /// <returns></returns>
+        public static double[,] BuildMatrix(int ranklevel, double[] pixeLlist)
+        {
+            double[,] result = new double[ranklevel, 1];
+
+            //a0
+            for (int i = 0; i < ranklevel; i++)
+            {
+                result[i, 0] = 1;
+            }
+            double[] powerlist = new double[ranklevel];
+            int j = 1;
+            while (j < ranklevel)
+            {
+                for (int i = 0; i < ranklevel; i++)
+                    powerlist[i] = Math.Pow(pixeLlist[i], j);
+                result = BuildAugmentedMatrix(result, powerlist);
+                j++;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// 给矩阵增加一列，或者说构建增广矩阵
+        /// </summary>
+        /// <param name="coefficients"></param>
+        /// <param name="constants"></param>
+        /// <returns></returns>
+        public static double[,] BuildAugmentedMatrix(double[,] coefficients, double[] constants)
+        {
+            int rows = coefficients.GetLength(0);
+            int cols = coefficients.GetLength(1) + 1; // 常数向量增加一列
+
+            double[,] augmentedMatrix = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    augmentedMatrix[i, j] = coefficients[i, j];
+                }
+                augmentedMatrix[i, cols - 1] = constants[i]; // 最后一列是常数向量
+            }
+
+            return augmentedMatrix;
+        }
+
+        public static double[,] AnotherBuildAugmentedMatrix(int ranklevel, double[] pixellist, double[] wavelengthlist)
+        {
+            double[,] matrix = BuildMatrix(ranklevel, pixellist);
+
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1) + 1; // 常数向量增加一列
+
+            double[,] augmentedMatrix = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols - 1; j++)
+                {
+                    augmentedMatrix[i, j] = matrix[i, j];
+                }
+                augmentedMatrix[i, cols - 1] = wavelengthlist[i]; // 最后一列是常数向量
+            }
+
+            return augmentedMatrix;
+        }
+
+
+        /// <summary>
+        /// 高斯消元法
+        /// </summary>
+        /// <param name="matrix"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static double[] GaussianElimination(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            double[] solution = new double[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                // Find pivot row and swap
+                int maxRowIndex = i;
+                for (int k = i + 1; k < rows; k++)
+                {
+                    if (Math.Abs(matrix[k, i]) > Math.Abs(matrix[maxRowIndex, i]))
+                    {
+                        maxRowIndex = k;
+                    }
+                }
+
+                // Swap rows i and maxRowIndex
+                if (maxRowIndex != i)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        double temp = matrix[i, j];
+                        matrix[i, j] = matrix[maxRowIndex, j];
+                        matrix[maxRowIndex, j] = temp;
+                    }
+                }
+
+                // Check if the pivot element is zero (singular matrix)
+                if (Math.Abs(matrix[i, i]) < double.Epsilon)
+                {
+                    throw new InvalidOperationException("Singular matrix");
+                }
+
+                // Reduce to triangular form
+                for (int j = i + 1; j < rows; j++)
+                {
+                    double factor = matrix[j, i] / matrix[i, i];
+                    for (int k = i; k < cols; k++)
+                    {
+                        matrix[j, k] -= factor * matrix[i, k];
+                    }
+                }
+            }
+
+            // Back substitution to solve the equations
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                double sum = 0;
+                for (int j = i + 1; j < rows; j++)
+                {
+                    sum += matrix[i, j] * solution[j];
+                }
+                solution[i] = (matrix[i, cols - 1] - sum) / matrix[i, i];
+            }
+
+            return solution;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using SpectrumAnalyzerPlus.Algorithms;
 
-namespace PolynomialRegressionSpectrometerCalibration
+namespace SpectrumAnalyzerPlus
 {
     class Program
     {
@@ -17,11 +18,11 @@ namespace PolynomialRegressionSpectrometerCalibration
 
             //new way
             int ranklevel = 3;
-            double[,] coefficient = AnotherBuildAugmentedMatrix(ranklevel, pixellist, wavelength);
+            double[,] coefficient = LinearAlgebra.AnotherBuildAugmentedMatrix(ranklevel, pixellist, wavelength);
             PrintMatrix(coefficient);
 
             // 使用高斯消元法求解
-            double[] solution = GaussianElimination(coefficient);
+            double[] solution = LinearAlgebra.GaussianElimination(coefficient);
 
             //target a0 = 164
             // 打印解向量
@@ -58,167 +59,6 @@ namespace PolynomialRegressionSpectrometerCalibration
             }
         }
 
-        // 构建矩阵的函数
-        #region Linear Algebra
-
-        /// <summary>
-        /// 根据峰数n构建n*n初始矩阵的函数
-        /// </summary>
-        /// <param name="pixeLlist">锋对应的像素序号</param>
-        /// <returns></returns>
-        static double[,] BuildMatrix(int ranklevel,double[] pixeLlist)
-        {
-            double[,] result = new double[ranklevel, 1];
-            ////a0
-            //for (int i = 0; i < n; i++)
-            //{
-            //    result[i, 0] = 1;
-            //}
-            ////a1
-            //result = BuildAugmentedMatrix(result, pixeLlist);
-            ////a2
-            //double[] a2list = new double[pixeLlist.Length];
-            //for (int i = 0; i < pixeLlist.Length; i++)
-            //{
-            //    a2list[i] = Math.Pow(pixeLlist[i], 2);
-            //}
-            //result = BuildAugmentedMatrix(result, a2list);
-            ////a3
-            //double[] a3list = new double[pixeLlist.Length];
-            //for (int i = 0; i < pixeLlist.Length; i++)
-            //{
-            //    a3list[i] = Math.Pow(pixeLlist[i], 3);
-            //}
-            //result = BuildAugmentedMatrix(result, a3list);
-
-            //a0
-            for (int i = 0; i < ranklevel; i++)
-            {
-                result[i, 0] = 1;
-            }
-            double[] powerlist = new double[ranklevel];
-            int j = 1;
-            while (j < ranklevel)
-            {
-                for (int i = 0; i < ranklevel; i++)
-                    powerlist[i]= Math.Pow(pixeLlist[i], j);
-                result = BuildAugmentedMatrix(result, powerlist);
-                j++;
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// 给矩阵增加一列，或者说构建增广矩阵
-        /// </summary>
-        /// <param name="coefficients"></param>
-        /// <param name="constants"></param>
-        /// <returns></returns>
-        static double[,] BuildAugmentedMatrix(double[,] coefficients, double[] constants)
-        {
-            int rows = coefficients.GetLength(0);
-            int cols = coefficients.GetLength(1) + 1; // 常数向量增加一列
-
-            double[,] augmentedMatrix = new double[rows, cols];
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols - 1; j++)
-                {
-                    augmentedMatrix[i, j] = coefficients[i, j];
-                }
-                augmentedMatrix[i, cols - 1] = constants[i]; // 最后一列是常数向量
-            }
-
-            return augmentedMatrix;
-        }
-
-        static double[,] AnotherBuildAugmentedMatrix(int ranklevel,double[] pixellist, double[] wavelengthlist)
-        {
-            double[,] matrix = BuildMatrix(ranklevel, pixellist);
-
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1) + 1; // 常数向量增加一列
-
-            double[,] augmentedMatrix = new double[rows, cols];
-
-            for (int i = 0; i < rows; i++)
-            {
-                for (int j = 0; j < cols - 1; j++)
-                {
-                    augmentedMatrix[i, j] = matrix[i, j];
-                }
-                augmentedMatrix[i, cols - 1] = wavelengthlist[i]; // 最后一列是常数向量
-            }
-
-            return augmentedMatrix;
-        }
-
-
-        // 高斯消元法
-        static double[] GaussianElimination(double[,] matrix)
-        {
-            int rows = matrix.GetLength(0);
-            int cols = matrix.GetLength(1);
-
-            double[] solution = new double[rows];
-
-            for (int i = 0; i < rows; i++)
-            {
-                // Find pivot row and swap
-                int maxRowIndex = i;
-                for (int k = i + 1; k < rows; k++)
-                {
-                    if (Math.Abs(matrix[k, i]) > Math.Abs(matrix[maxRowIndex, i]))
-                    {
-                        maxRowIndex = k;
-                    }
-                }
-
-                // Swap rows i and maxRowIndex
-                if (maxRowIndex != i)
-                {
-                    for (int j = 0; j < cols; j++)
-                    {
-                        double temp = matrix[i, j];
-                        matrix[i, j] = matrix[maxRowIndex, j];
-                        matrix[maxRowIndex, j] = temp;
-                    }
-                }
-
-                // Check if the pivot element is zero (singular matrix)
-                if (Math.Abs(matrix[i, i]) < double.Epsilon)
-                {
-                    throw new InvalidOperationException("Singular matrix");
-                }
-
-                // Reduce to triangular form
-                for (int j = i + 1; j < rows; j++)
-                {
-                    double factor = matrix[j, i] / matrix[i, i];
-                    for (int k = i; k < cols; k++)
-                    {
-                        matrix[j, k] -= factor * matrix[i, k];
-                    }
-                }
-            }
-
-            // Back substitution to solve the equations
-            for (int i = rows - 1; i >= 0; i--)
-            {
-                double sum = 0;
-                for (int j = i + 1; j < rows; j++)
-                {
-                    sum += matrix[i, j] * solution[j];
-                }
-                solution[i] = (matrix[i, cols - 1] - sum) / matrix[i, i];
-            }
-
-            return solution;
-        }
-
-        #endregion
     }
 }
 
