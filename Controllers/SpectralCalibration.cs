f6614747b04458d875d979cd40093d2306c1d6f2
MatinxHT <linduanpeng@live.com>
2024-04-01 15:44:38 +0800

    增加波长校准的控制器

--- Controllers/SpectralCalibration.cs
+++ Controllers/SpectralCalibration.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SpectrumAnalyzerPlus.Algorithms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,38 @@ namespace SpectrumAnalyzerPlus.Controllers
     /// </summary>
     public class SpectralCalibration
     {
+        public static (bool,double[]) WavelengthCalibration(int ranklevel,double[] data, List<double> standardWavelength)
+        {
+            List<int> pixels = SpectralProcessing.DerivativeFindPeaks(data);
+            double[] pixellist = new double[pixels.Count];
+            double[] standardWavelengthlist = new double[standardWavelength.Count];
+            double[,] coefficient = LinearAlgebra.AnotherBuildAugmentedMatrix(ranklevel, pixellist, standardWavelengthlist);
+            // 使用高斯消元法求解
+            double[] solution = LinearAlgebra.GaussianElimination(coefficient);
+
+            //创建波长检查数组
+            double[] wavelengthCheck = new double[data.Length];
+            for(int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < solution.Length; j++)
+                {
+                    wavelengthCheck[i] += solution[j] * Math.Pow(i, j);
+                }
+            }
+
+            for(int i = 0;i < wavelengthCheck.Length - 1; i++)
+            {
+                if (wavelengthCheck[i + 1] - wavelengthCheck[i] < 1)
+                {
+                    ranklevel -= 1;
+                    double[] realrank = new double[ranklevel];
+                    return (false, realrank);
+                }
+            }
+
+
+            return (true,solution);
+        }
 
     }
 }
